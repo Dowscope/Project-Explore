6f8a3f6b6f37a5f7cfd9dc921cd3142a313ea2f9
Timothy Dowling <timothy.dowling@me.com>
2021-02-12 15:11:29 -0500

    Starting Gameloop

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #define WINDOW_HEIGHT 600
 
 /* Declare Functions */
+void initialize();
 void shutdown();
 
 /* Globals */
@@ -21,13 +22,22 @@ char* program_title = "Project Explore";
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Starting " << program_title << " Ver. " << VERSION << std::endl;
-    screen = new Screen(WINDOW_WIDTH, WINDOW_HEIGHT, program_title);
+    initialize();
     shutdown();
     return 0;
 }
 
+void initialize()
+{
+    std::cout << "Initializing " << program_title << " Ver. " << VERSION << std::endl;
+    screen = new Screen(WINDOW_WIDTH, WINDOW_HEIGHT, program_title);
+}
+
 void shutdown()
 {
+    std::cout << "Starting Shutdown..." << std::endl;
+    screen = new Screen(WINDOW_WIDTH, WINDOW_HEIGHT, program_title);
     delete screen;
+
+    std::cout << "Shutdown Completed" << std::endl;
 }
